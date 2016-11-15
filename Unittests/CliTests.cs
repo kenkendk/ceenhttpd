5593e606da594c2ade976021645d0c508a2745c2
Kenneth Skovhede <kenneth@hexad.dk>
2016-11-15 21:52:03 +0100

    Added basic unittest for parsing the config file

---
+++ Unittests/CliTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading.Tasks;
+using Ceen;
+using Ceen.Httpd;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace Unittests
+{
+	public class TimeOfDayHandler : IHttpModule
+	{
+		public TimeOfDayHandler(int a, string b, bool c)
+		{
+		}
+
+		#region IHttpModule implementation
+		public async Task<bool> HandleAsync(IHttpContext context)
+		{
+			context.Response.SetNonCacheable();
+			await context.Response.WriteAllJsonAsync(JsonConvert.SerializeObject(new { time = DateTime.Now.TimeOfDay }));
+			return true;
+		}
+		#endregion
+
+		public bool ExtendedLogging { get; set; }
+		public int MaxFileSize { get; set; }
+	}
+
+	public class TestLogger : ILogger
+	{
+		public Task LogRequest(IHttpContext context, Exception ex, DateTime started, TimeSpan duration)
+		{
+			return Task.FromResult(true);
+		}
+
+		public bool TestProp { get; set; }
+	}
+
+
+
+	[TestFixture()]
+	public class CliTests
+	{
+		[Test()]
+		public void TestParseConfig()
+		{
+			var filepath = System.IO.Path.Combine(
+				System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location)
+				, "test1.txt");
+			var c1 = Ceen.Httpd.Cli.ConfigParser.ParseTextFile(filepath);
+
+			Assert.AreEqual(c1.CertificatePath, "xyz");
+			Assert.AreEqual(c1.CertificatePassword, "pass");
+			Assert.AreEqual(c1.HttpPort, 22);
+			Assert.AreEqual(c1.HttpsPort, 333);
+			Assert.AreEqual(c1.HttpAddress, "0.0.0.0");
+			Assert.AreEqual(c1.HttpsAddress, "any");
+}
+	}
+}
