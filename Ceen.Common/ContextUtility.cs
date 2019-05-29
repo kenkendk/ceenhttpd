24b807d6437fcf9b4844c16e4a1cf8a85c98ff0c
Kenneth Skovhede <kenneth@hexad.dk>
2019-05-29 22:51:45 +0200

    Added helper methods to log messages on the current context

--- Ceen.Common/ContextUtility.cs
+++ Ceen.Common/ContextUtility.cs
@@ -52,5 +52,100 @@ namespace Ceen
         /// </summary>
         public static IDictionary<string, string> LogData => m_activeContext.Value?.LogData;
 
+        /// <summary>
+        /// Logs a message
+        /// </summary>
+        /// <param name="level">The level to log</param>
+        /// <param name="message">The message to log</param>
+        /// <param name="ex">The exception to log</param>
+        /// <returns>An awaitable task</returns>
+        public static Task LogMessageAsync(LogLevel level, string message, Exception ex)
+        {
+            return Current.LogMessageAsync(level, message, ex);
+        }
+
+        /// <summary>
+        /// Logs a debug message
+        /// </summary>
+        /// <param name="message">The message to log</param>
+        /// <param name="ex">The exception to log</param>
+        /// <returns>An awaitable task</returns>
+        public static Task LogDebugAsync(string message, Exception ex = null)
+        {
+            return Current.LogDebugAsync(message, ex);
+        }
+
+        /// <summary>
+        /// Logs a debug message
+        /// </summary>
+        /// <param name="ex">The exception to log</param>
+        /// <returns>An awaitable task</returns>
+        public static Task LogDebugAsync(Exception ex)
+        {
+            return Current.LogDebugAsync(null, ex);
+        }        
+
+        /// <summary>
+        /// Logs an error message
+        /// </summary>
+        /// <param name="message">The message to log</param>
+        /// <param name="ex">The exception to log</param>
+        /// <returns>An awaitable task</returns>
+        public static Task LogErrorAsync(string message, Exception ex = null)
+        {
+            return Current.LogErrorAsync(message, ex);
+        }
+
+        /// <summary>
+        /// Logs an error message
+        /// </summary>
+        /// <param name="ex">The exception to log</param>
+        /// <returns>An awaitable task</returns>
+        public static Task LogErrorAsync(Exception ex)
+        {
+            return Current.LogErrorAsync(null, ex);
+        }
+
+        /// <summary>
+        /// Logs an information message
+        /// </summary>
+        /// <param name="message">The message to log</param>
+        /// <param name="ex">The exception to log</param>
+        /// <returns>An awaitable task</returns>
+        public static Task LogInformationAsync(string message, Exception ex = null)
+        {
+            return Current.LogInformationAsync(message, ex);
+        }
+
+        /// <summary>
+        /// Logs an information message
+        /// </summary>
+        /// <param name="ex">The exception to log</param>
+        /// <returns>An awaitable task</returns>
+        public static Task LogInformationAsync(Exception ex)
+        {
+            return Current.LogInformationAsync(null, ex);
+        }
+
+        /// <summary>
+        /// Logs a warning message
+        /// </summary>
+        /// <param name="message">The message to log</param>
+        /// <param name="ex">The exception to log</param>
+        /// <returns>An awaitable task</returns>
+        public static Task LogWarningAsync(string message, Exception ex = null)
+        {
+            return Current.LogWarningAsync(message, ex);
+        }
+
+        /// <summary>
+        /// Logs a warning message
+        /// </summary>
+        /// <param name="ex">The exception to log</param>
+        /// <returns>An awaitable task</returns>
+        public static Task LogWarningAsync(Exception ex)
+        {
+            return Current.LogWarningAsync(null, ex);
+        }
     }
 }
