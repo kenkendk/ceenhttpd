ec1c683f293da835b481e5680f8b0753afa2c0ca
Kenneth Skovhede <kenneth@hexad.dk>
2019-11-25 21:35:29 +0100

    Added `Ceen.Extras` as a package to build

--- NuGet/build.sh
+++ NuGet/build.sh
@@ -62,6 +62,7 @@ done
 "${XBUILD}" "/p:Configuration=Release" "../Ceen.sln"
 
 find . -type f -name "*.dll" | xargs rm
+cp ../Ceen.Extras/bin/Release/netstandard2.0/*.dll .
 cp ../Ceen.Security/bin/Release/netstandard2.0/*.dll .
 cp ../Ceen.Httpd.Cli/bin/Release/netstandard2.0/*.dll .
 
@@ -69,13 +70,15 @@ SED_EXPR="s/.*\<version\>.*/    \<version\>${VERSION}\<\/version\>/"
 SED_EXPR2="s/.*\<dependency.*id\=\"Ceen.Httpd\".*/        \<dependency id\=\"Ceen.Httpd\" version\=\"${VERSION}\" \/\>/"
 SED_EXPR3="s/.*\<dependency.*id\=\"Ceen.Mvc\".*/        \<dependency id\=\"Ceen.Mvc\" version\=\"${VERSION}\" \/\>/"
 SED_EXPR4="s/.*\<dependency.*id\=\"Ceen.Database\".*/        \<dependency id\=\"Ceen.Database\" version\=\"${VERSION}\" \/\>/"
+SED_EXPR5="s/.*\<dependency.*id\=\"Ceen.Common\".*/        \<dependency id\=\"Ceen.Common\" version\=\"${VERSION}\" \/\>/"
 for NUSPEC in $(find . -type f -name "*.nuspec"); do
 	sed "${SED_EXPR}" "${NUSPEC}" > "tmp01.tmp"
 	sed "${SED_EXPR2}" "tmp01.tmp" > "tmp02.tmp"
 	sed "${SED_EXPR3}" "tmp02.tmp" > "tmp03.tmp"
-	sed "${SED_EXPR4}" "tmp03.tmp" > "${NUSPEC}.tmp.nuspec"
+	sed "${SED_EXPR3}" "tmp03.tmp" > "tmp04.tmp"
+	sed "${SED_EXPR4}" "tmp04.tmp" > "${NUSPEC}.tmp.nuspec"
 	"${NUGET}" "pack" "${NUSPEC}.tmp.nuspec"
-	rm "tmp01.tmp" "tmp02.tmp" "tmp03.tmp" "${NUSPEC}.tmp.nuspec"
+	rm "tmp01.tmp" "tmp02.tmp" "tmp03.tmp" "tmp04.tmp" "${NUSPEC}.tmp.nuspec"
 done
 
 rm *.dll
