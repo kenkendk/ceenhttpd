1d53c40766c90d9149dbbb429ef08c98fdffb0f0
Kenneth Skovhede <kenneth@hexad.dk>
2020-05-15 08:55:18 +0200

    Documentation update

--- Ceen.Extras/RequireSecureQueueHandler.cs
+++ Ceen.Extras/RequireSecureQueueHandler.cs
@@ -8,13 +8,25 @@ namespace Ceen.Extras
     /// </summary>
     public class RequireSecureQueueHandler : IHttpModule
     {
+        /// <summary>
+        /// The name of the queue to check for
+        /// </summary>
         private readonly string m_queuename;
 
+        /// <summary>
+        /// Creates a new queue handler checker
+        /// </summary>
+        /// <param name="queuename">The name of the queue to check for</param>
         public RequireSecureQueueHandler(string queuename)
         {
             m_queuename = queuename ?? throw new ArgumentNullException(nameof(queuename));
         }
 
+        /// <summary>
+        /// Handles the request by checking for the secure queue header value
+        /// </summary>
+        /// <param name="context">The execution context</param>
+        /// <returns><c>true</c> if the request was rejected, <c>false</c> otherwise</returns>
         public Task<bool> HandleAsync(IHttpContext context)
         {
             var queue = QueueModule.GetQueue(m_queuename);
